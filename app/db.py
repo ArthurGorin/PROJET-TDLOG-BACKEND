4c1b080d0ad5f4b43bf481ca01585bfb72dfebb1
Gorin Arthur <arthur.gorin@enpc.fr>
2025-12-29 19:02:34 +0100

    explication

--- app/db.py
+++ app/db.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = 'sqlite:///./app.db'
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+DATABASE_URL = 'sqlite:///./app.db' #la db est stocké à la racine du back dans app.db
+engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False}) #moteur sql
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) #une conversation avec la DB, chaque requete http aura sa propre session
+Base = declarative_base() #classe mère de toutes les tables
 
-def get_db():
+def get_db(): #dépendance fast api : ouvre une session db la donne à la route puis la ferme automatiquement
     db = SessionLocal()
     try:
         yield db
--- app/main.py
+++ app/main.py
@@ -1,27 +1,30 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware 
 
 from .routers import auth, events, tickets, scan, admin, students, participants
 from .db import Base, engine
 from .initial_superadmin import ensure_initial_superadmin
 
-# Création des tables au démarrage (simple pour dev)
+# Création des tables au démarrage
 Base.metadata.create_all(bind=engine)
-ensure_initial_superadmin()
 
+#garantit qu'au moins un admin existe (si nouvelle db)
+ensure_initial_superadmin() 
+
+#création de l'app centrale
 app = FastAPI(title="TD-LOG API", version="0.1.0")
 
-#Quand on lance le backend
+#route de base Quand on lance le backend
 @app.get("/")
 def root():
     return {"message": "Backend TDLOG running"}
 
-#petit test arthur sur la partie participant 
+#petit test initial
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-
+#autorise le frontend à appeler le backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # en dev : on autorise tout, on durcira plus tard si besoin
@@ -30,6 +33,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#branchement des routes
 app.include_router(auth.router)
 app.include_router(events.router)
 app.include_router(tickets.router)
